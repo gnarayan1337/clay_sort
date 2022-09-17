70eb525ce63e2b341db26c235c892f4174d406eb
Jakob McPherson <jakob@mcpherson.org>
2022-09-16 20:59:45 -0500

    Changed from recursive to iterative strategy (noticeable improvement)

--- claysort.cpp
+++ claysort.cpp
@@ -6,18 +6,17 @@
 using namespace std;
 
 // prototypes
-void combine(vector<string> &array, int start, int end);
-void claysort(vector<string> &array, int start, int end);
+//void combine(vector<string> &array, int start, int cut, int end);
+//void claysort(vector<string> &array, int start, int end);
 
 // don't touch anything above this
-void combine(vector<string> &array, int start, int end) {
+void combine(vector<string> &array, int start, int cut, int end) {
   int i = start; // pointer that points to start
-  int mid = (start + end) / 2; // pointer that points to middle of array
-  int j = mid + 1; // pointer that points to mid + 1
+  int j = cut + 1; // pointer that points to mid + 1
 
   vector<string> temp;
 
-  while (i <= mid and j <= end) {
+  while (i <= cut and j <= end) {
     if (array[i] < array[j]) {
       temp.push_back(array[i]);
       i++;
@@ -29,7 +28,7 @@ void combine(vector<string> &array, int start, int end) {
   }
 
   // copy remainng elements from first array
-  while (i <= mid) {
+  while (i <= cut) {
     temp.push_back(array[i++]);
   }
 
@@ -46,18 +45,18 @@ void combine(vector<string> &array, int start, int end) {
   return;
 }
   
-void claysort(vector<string> &array, int start, int end) {
-  // base case
-  if (start >= end) // we don't have any limits or we just have one element we need to sort
-    return; // array containing 1 element is a sorted array
+// void claysort(vector<string> &array, int start, int end) {
+//   // base case
+//   if (start >= end) // we don't have any limits or we just have one element we need to sort
+//     return; // array containing 1 element is a sorted array
 
-  // rec case
-  int mid = (start + end) / 2;
-  claysort(array, start, mid);
-  claysort(array, mid + 1, end);
-  // once the left part and right part are sorted we combine them into a single array
-  return combine(array, start, end);
-}
+//   // rec case
+//   int mid = (start + end) / 2;
+//   claysort(array, start, mid);
+//   claysort(array, mid + 1, end);
+//   // once the left part and right part are sorted we combine them into a single array
+//   return combine(array, start, end);
+// }
 
 int main() {
   cin.tie(nullptr) -> ios_base::sync_with_stdio(false);
@@ -75,33 +74,27 @@ int main() {
   }
 
   //claysort(array, start, end);
-  
-  int start = 0;
-  int end = array.size() - 1;
-  int mid = (start + end) / 2;
-  int q1 = (start + mid) / 2;
-  int q3 = (mid + end) / 2;
-  int e1 = (start + q1) / 2;
-  int e3 = (q1 + mid) / 2;
-  int e5 = (mid + q3) / 2;
-  int e7 = (q3 + end) / 2;
-    
-  claysort(array, start, e1);
-  claysort(array, e1 + 1, q1);
-  combine(array, start, q1);
-  claysort(array, q1 + 1, e3);
-  claysort(array, e3 + 1, mid);
-  combine(array, q1, mid);
-  combine(array, start, mid);
-  claysort(array, mid + 1, e5);
-  claysort(array, e5 + 1, q3);
-  combine(array, mid, q3);
-  claysort(array, q3 + 1, e7);
-  claysort(array, e7 + 1, end);
-  combine(array, q3, end);
-  combine(array, mid, end);
-  combine(array, start, end);
 
+  //the following code iteratively sorts larger and larger sections of the array at a time
+  int windowSize = 2;
+  int size = array.size();
+  
+  while (windowSize / 2 <= size) {
+    int start = 0;
+    int end = start + windowSize - 1;
+    int cut = (start + end) / 2;
+    while (end <= size - 1) {
+      combine(array, start, cut, end);
+      start += windowSize;
+      cut += windowSize;
+      end += windowSize;
+    }
+    cut = start + windowSize / 2 - 1;
+    end = size - 1;
+    combine(array, start, cut, end);
+    windowSize *= 2;
+  }
+  
   for (auto x : array) {
     cout << x << "\n";
   }
