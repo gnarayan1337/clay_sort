4df7a2413aefc2824f5a4faed558f7f87be62ef1
jakob-mcpherson <70408370+jakob-mcpherson@users.noreply.github.com>
2022-10-05 11:52:20 -0500

    Update sortanalysis.sh
    Fixed typos

--- sortanalysis.sh
+++ sortanalysis.sh
@@ -1,20 +1,20 @@
-cat ../../ordered.txt | head -n 1 > ordered-10e2.txt
-cat ../../ordered.txt | head -n 10 > ordered-10e2.txt
+cat ../../ordered.txt | head -n 1 > ordered-10e0.txt
+cat ../../ordered.txt | head -n 10 > ordered-10e1.txt
 cat ../../ordered.txt | head -n 100 > ordered-10e2.txt
 cat ../../ordered.txt | head -n 1000 > ordered-10e3.txt
 cat ../../ordered.txt | head -n 10000 > ordered-10e4.txt
 cat ../../ordered.txt | head -n 100000 > ordered-10e5.txt
 
-cat ../../ordered.txt | head -n 1 > reverse-ordered-10e0.txt
-cat ../../ordered.txt | head -n 10 > reverse-ordered-10e1.txt
-cat ../../ordered.txt | head -n 100 > reverse-ordered-10e2.txt
-cat ../../ordered.txt | head -n 1000 > reverse-ordered-10e3.txt
-cat ../../ordered.txt | head -n 10000 > reverse-ordered-10e4.txt
-cat ../../ordered.txt | head -n 100000 > reverse-ordered-10e5.txt
+cat ../../reverse-ordered.txt | head -n 1 > reverse-ordered-10e0.txt
+cat ../../reverse-ordered.txt | head -n 10 > reverse-ordered-10e1.txt
+cat ../../reverse-ordered.txt | head -n 100 > reverse-ordered-10e2.txt
+cat ../../reverse-ordered.txt | head -n 1000 > reverse-ordered-10e3.txt
+cat ../../reverse-ordered.txt | head -n 10000 > reverse-ordered-10e4.txt
+cat ../../reverse-ordered.txt | head -n 100000 > reverse-ordered-10e5.txt
 
-cat ../../ordered.txt | head -n 1 > random-ordered-10e0.txt
-cat ../../ordered.txt | head -n 10 > random-ordered-10e1.txt
-cat ../../ordered.txt | head -n 100 > random-ordered-10e2.txt
-cat ../../ordered.txt | head -n 1000 > random-ordered-10e3.txt
-cat ../../ordered.txt | head -n 10000 > random-ordered-10e4.txt
-cat ../../ordered.txt | head -n 100000 > random-ordered-10e5.txt
+cat ../../random.txt | head -n 1 > random-ordered-10e0.txt
+cat ../../random.txt | head -n 10 > random-ordered-10e1.txt
+cat ../../random.txt | head -n 100 > random-ordered-10e2.txt
+cat ../../random.txt | head -n 1000 > random-ordered-10e3.txt
+cat ../../random.txt | head -n 10000 > random-ordered-10e4.txt
+cat ../../random.txt | head -n 100000 > random-ordered-10e5.txt
